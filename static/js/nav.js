8d37d791f092e4bddd0b559a7db500d1f8b024df
bogdantro <sabertoothtri@gmail.com>
2026-02-24 21:26:20 +0100

    s

--- static/js/nav.js
+++ static/js/nav.js
@@ -24,7 +24,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const triggers = document.querySelectorAll("#sidebar .mob-dd-trigger");
 
+  triggers.forEach(btn => {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = btn.getAttribute("data-target");
+      const panel = document.getElementById(targetId);
+      if (!panel) return;
+
+      // toggle this panel
+      panel.classList.toggle("open");
+      btn.classList.toggle("open");
+    });
+  });
+});
 
 /////////// MOBILE JAVASCRIPT
 // MENU
@@ -32,21 +48,20 @@ function menu(){
   const sidebar = document.getElementById('sidebar');
   const navbar = document.getElementById('mobileNav');
   const hamburger = document.getElementById('hamburger');
-
   const dropdown = document.getElementById('sidebarServDrop');
-
   const body = document.getElementsByTagName('body')[0];
 
   if (window.getComputedStyle(sidebar,null).getPropertyValue("opacity") == '0'){
-    navbar.classList.add('menu')  
-    hamburger.classList.add('click')  
-    sidebar.classList.add('active')
+    navbar.classList.add('menu');
+    hamburger.classList.add('click');
+    sidebar.classList.add('active');
     body.style.overflowY = 'hidden';
-    dropdown.classList.remove('active')
+    if (dropdown) dropdown.classList.remove('active');
   } else{
-    navbar.classList.remove('menu') 
-    hamburger.classList.remove('click')  
-    sidebar.classList.remove('active')
-  }  
+    navbar.classList.remove('menu');
+    hamburger.classList.remove('click');
+    sidebar.classList.remove('active');
+    body.style.overflowY = 'auto';
+  }
 };
 
