9175e49aa6aeea5988cd24db99b21e98f7401d2e
bogdantro <sabertoothtri@gmail.com>
2026-04-14 18:49:04 +0200

    d

--- apps/userprofile/urls.py
+++ apps/userprofile/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LogoutView
 from .views import StaffLoginView, myaccount
 
 urlpatterns = [
-    path("login/", StaffLoginView.as_view(), name="login"),
+    path("lfk/kundebruker/secure/login/ektelogin/", StaffLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("myaccount/", myaccount, name="myaccount"),
+    path("lfk/kundebruker/secure/login/", myaccount, name="myaccount"),
 ]
